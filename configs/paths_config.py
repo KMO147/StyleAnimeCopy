2e9a27cea2ce740eacaa8817b59ba49eff265cc8
KMO147 <100119289+KMO147@users.noreply.github.com>
2023-11-19 21:51:22 +0700

    Add files via upload

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,21 +1,21 @@
 dataset_paths = {
-	'anime_train_segmentation': '/mnt/lab/lzs/psp_anime_4921/anime/anime_seg_train',
-	'anime_test_segmentation': '/mnt/lab/lzs/psp_anime_4921/anime/anime_seg_test_68',
-	'anime_train': '/mnt/lab/lzs/psp_anime_4921/anime/anime_face_train',
-	'anime_test': '/mnt/lab/lzs/psp_anime_4921/anime/anime_face_test_68',
+	'anime_train_segmentation': '/kaggle/working/dds/Danbooru-Parsing/anime_seg_train',
+	'anime_test_segmentation': '/kaggle/working/dds/Danbooru-Parsing/anime_seg_test_68',
+	'anime_train': '/kaggle/working/dds/Danbooru-Parsing/anime_face_train',
+	'anime_test': '/kaggle/working/dds/Danbooru-Parsing/anime_face_test_68',
     
-	'face_train_segmentation': '/mnt/lab/lzs/psp_anime_4921/celeba/celeba_seg_train',
-	'face_test_segmentation': '/mnt/lab/lzs/psp_anime_4921/celeba/celeba_seg_test_68',
-	'face_train': '/mnt/lab/lzs/psp_anime_4921/celeba/celeba_face_train',
-	'face_test': '/mnt/lab/lzs/psp_anime_4921/celeba/celeba_face_test_68',
+	'face_train_segmentation': '/kaggle/working/dds/CelebaMask-HQ/celeba_seg_train',
+	'face_test_segmentation': '/kaggle/working/dds/CelebaMask-HQ/celeba_seg_test_68',
+	'face_train': '/kaggle/working/dds/CelebaMask-HQ/celeba_face_train',
+	'face_test': '/kaggle/working/dds/CelebaMask-HQ/celeba_face_test_68',
 }
 
 model_paths = {
-	'anime_ffhq': 'pretrained_models/stylegan2_anime_pretrained.pt',
-	'ir_se50': 'pretrained_models/model_ir_se50.pth',
-	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
-	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
-	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
-	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+	'anime_ffhq': '/kaggle/working/pretrained_models/stylegan2_anime_pretrained.pt',
+	'ir_se50': '/kaggle/working/pretrained_models/model_ir_se50.pth',
+	'circular_face': '/kaggle/working/pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': '/kaggle/working/pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': '/kaggle/working/pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': '/kaggle/working/pretrained_models/mtcnn/onet.npy',
 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat'
 }

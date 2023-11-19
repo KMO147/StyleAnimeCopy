ee86d4d8fdec5800e583b7dad341df672d8a1cec
KMO147 <100119289+KMO147@users.noreply.github.com>
2023-11-19 23:36:35 +0700

    Add files via upload

--- training/coach.py
+++ training/coach.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 from utils import common, train_utils
 from criteria import id_loss, w_norm
 from configs import data_configs
-from datasets.images_dataset import ImagesDataset
+from dataset.images_dataset import ImagesDataset
 from criteria.lpips.lpips import LPIPS
 from models.styleAnime import StyleAnime
 

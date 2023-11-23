29a21b0ac084cdf94c052df75145a9b47a2fda9e
KMO147 <100119289+KMO147@users.noreply.github.com>
2023-11-24 01:07:53 +0700

    Add files via upload

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,21 +1,22 @@
+path = '/kaggle/input/dataset'
 dataset_paths = {
-	'anime_train_segmentation': 'dds/Danbooru-Parsing/anime_seg_train',
-	'anime_test_segmentation': 'dds/Danbooru-Parsing/anime_seg_test_68',
-	'anime_train': 'dds/Danbooru-Parsing/anime_face_train',
-	'anime_test': 'dds/Danbooru-Parsing/anime_face_test_68',
+	'anime_train_segmentation': path + 'dds/Danbooru-Parsing/anime_seg_train',
+	'anime_test_segmentation': path + 'dds/Danbooru-Parsing/anime_seg_test_68',
+	'anime_train': path + 'dds/Danbooru-Parsing/anime_face_train',
+	'anime_test': path + 'dds/Danbooru-Parsing/anime_face_test_68',
     
-	'face_train_segmentation': 'dds/CelebaMask-HQ/celeba_seg_train',
-	'face_test_segmentation': 'dds/CelebaMask-HQ/celeba_seg_test_68',
-	'face_train': 'dds/CelebaMask-HQ/celeba_face_train',
-	'face_test': 'dds/CelebaMask-HQ/celeba_face_test_68',
+	'face_train_segmentation': path + 'dds/CelebaMask-HQ/celeba_seg_train',
+	'face_test_segmentation': path + 'dds/CelebaMask-HQ/celeba_seg_test_68',
+	'face_train': path + 'dds/CelebaMask-HQ/celeba_face_train',
+	'face_test': path + 'dds/CelebaMask-HQ/celeba_face_test_68',
 }
 
 model_paths = {
-	'anime_ffhq': 'pretrained_models/stylegan2_anime_pretrained.pt',
-	'ir_se50': 'pretrained_models/model_ir_se50.pth',
-	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
-	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
-	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
-	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+	'anime_ffhq': path + 'pretrained_models/stylegan2_anime_pretrained.pt',
+	'ir_se50': path + 'pretrained_models/model_ir_se50.pth',
+	'circular_face': path + 'pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': path + 'pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': path + 'pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': path + 'pretrained_models/mtcnn/onet.npy',
 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat'
 }
